c1f805f35ddac172a808a25f20162d0cf214500d
wolyshaw <shaw@xwlong.com>
2017-06-08 16:27:02 +0800

    fix less

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+import { Button, notification } from 'antd'
+
+import 'antd/dist/antd.less'
+
+const openNotification = () => {
+	notification.open({
+		message: 'Notification Title',
+		description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+	});
+};
+
 const App = props => {
-  return (
-    <div>app</div>
-  )
+	return (
+		<div>
+			<Button type="primary" onClick={openNotification}>Open the notification box</Button>
+		</div>
+	)
 }
 
 render(
-  <App/>,
-  document.getElementById('app')
+	<App/>,
+	document.getElementById('app')
 )
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,56 +10,65 @@ const hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true'
 const setResolve = dirname => path.resolve(path.join(__dirname, 'src', dirname))
 
 module.exports = {
-  entry: ['./src/index.js', hotMiddlewareScript],
-  output: {
-    filename: 'index.js',
-    path: path.resolve('./dev'),
-    publicPath: publicPath
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets:  ['react', 'es2015']
-        }
-      },
-      {
-        test: /\.css$|\.less$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader!less-loader?modules&localIdentName=[name]--[local]--[hash:base64:5]']
-        })
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          name: '[name].[ext]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      util: setResolve('util'),
-      static: setResolve('static'),
-      actions: setResolve('actions'),
-      containers: setResolve('containers'),
-      components: setResolve('components')
-    }
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'static', 'index_default.html'),
-      title: config.site.title,
-      keywords: config.site.keywords,
-      description: config.site.description
-    }),
-    new ExtractTextPlugin('static/styles.css')
-  ]
+	entry: ['./src/index.js', hotMiddlewareScript],
+	output: {
+		filename: 'index.js',
+		path: path.resolve('./dev'),
+		publicPath: publicPath
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets:  ['react', 'es2015']
+				}
+			},
+			{
+				test: /\.(css|less)$/,
+				exclude: /node_modules/,
+				loader: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader?modules&localIdentName=[name]--[local]--[hash:base64:5]', 'less-loader']
+				})
+			},
+			{
+				test: /\.(css|less)$/,
+				include: /node_modules/,
+				loader: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader!less-loader'
+				})
+			},
+			{
+				test: /\.(jpg|png|gif)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 8192,
+					name: '[name].[ext]'
+				}
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			util: setResolve('util'),
+			static: setResolve('static'),
+			actions: setResolve('actions'),
+			containers: setResolve('containers'),
+			components: setResolve('components')
+		}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src', 'static', 'index_default.html'),
+			title: config.site.title,
+			keywords: config.site.keywords,
+			description: config.site.description
+		}),
+		new ExtractTextPlugin('static/styles.css')
+	]
 }
